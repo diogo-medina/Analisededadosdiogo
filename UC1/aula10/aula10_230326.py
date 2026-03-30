6b8166b5d54078684dbc8929ae4f1aa56cda4b26
diogo <diogodurao10@gmail.com>
2026-03-30 18:27:02 -0300

    adicionando

--- UC1/aula10/aula10_230326.py
+++ UC1/aula10/aula10_230326.py
@@ -1,25 +1,25 @@
-# # # --- 1. Importação e Verificação do Pandas ---
+# --- 1. Importação e Verificação do Pandas ---
 
-# # import sys # Usaremos o sys para ver a versão do Python
-# # # A convenção é "import pandas as pd"
-# # try:
-# #  print("Tentando importar a biblioteca Pandas...")
-# #  import pandas as pd
-# #  print("Pandas importado com sucesso!")
+import sys # Usaremos o sys para ver a versão do Python
+# A convenção é "import pandas as pd"
+try:
+ print("Tentando importar a biblioteca Pandas...")
+ import pandas as pd
+ print("Pandas importado com sucesso!")
  
-# #  # É uma boa prática verificar a versão
-# #  print(f"Versão do Pandas instalada: {pd.__version__}")
-# #  print(f"Versão do Python: {sys.version.split()[0]}")
+ # É uma boa prática verificar a versão
+ print(f"Versão do Pandas instalada: {pd.__version__}")
+ print(f"Versão do Python: {sys.version.split()[0]}")
  
-# # except ImportError:
-# #  print("\n--- ERRO ---")
-# #  print("A biblioteca Pandas não foi encontrada.")
-# #  print("Por favor, instale-a usando o comando no seu terminal:")
-# #  print("pip install pandas")
-# # except Exception as e:
-# #  print(f"Ocorreu um erro inesperado: {e}")
-# # finally:
-# #  print("Verificação de setup concluída.\n")
+except ImportError:
+ print("\n--- ERRO ---")
+ print("A biblioteca Pandas não foi encontrada.")
+ print("Por favor, instale-a usando o comando no seu terminal:")
+ print("pip install pandas")
+except Exception as e:
+ print(f"Ocorreu um erro inesperado: {e}")
+finally:
+ print("Verificação de setup concluída.\n")
 
 
 # # # --- 2. Criando uma Série (Estrutura 1D) ---
